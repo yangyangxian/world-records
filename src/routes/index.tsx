5c4dbc1daea1ed2725a5b00f1830b2629a7b052f
Yang Yang <persistence@vip.qq.com>
2025-12-17 14:02:22 +0800

    feat: make the elements in the center of screen
    Notes:
    - Here is the interesting thing: if we don't add axis center to parent container, the width of the child element is the width of the parent width. But if we add axis center to the paren, the width of the child is shrinking to the width of content in the child element.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,7 @@ export const Route = createFileRoute('/')({ component: App })
 
 function App() {
   return (
-    <div className='flex center text-2xl font-bold'>
+    <div className='flex flex-col center text-2xl font-bold bg-cyan-500 text-white h-screen'>
       <div>Hello, world.</div>
       <div>Welcome to World Records!</div>
     </div>
