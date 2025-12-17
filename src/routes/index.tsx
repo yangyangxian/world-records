5246b6d4b6a8e6cab755fe3e135f044317355f50
Yang Yang <persistence@vip.qq.com>
2025-12-17 13:42:15 +0800

    feat: added a basic elements structure
    Notes:
    - The document height is actually determined by the content in it if the height is not explicitly specified.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,9 @@ export const Route = createFileRoute('/')({ component: App })
 
 function App() {
   return (
-    <div>Hello, world.</div>
+    <div className='flex center text-2xl font-bold'>
+      <div>Hello, world.</div>
+      <div>Welcome to World Records!</div>
+    </div>
   )
 }
