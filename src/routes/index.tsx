788b485229f47483ade433bfbc54b1d358814338
Yang Yang <persistence@vip.qq.com>
2025-12-19 18:21:22 +0800

    chore: use grid layout
    Notes:
    - Actually the best elegant way to position the child elements is to use the grid display. Grid display which is top down approach can specify the position of its children. However, in flex layout we need to add margin to the child elements.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,9 +4,9 @@ export const Route = createFileRoute('/')({ component: App })
 
 function App() {
   return (
-    <div className='flex flex-col center text-2xl font-bold bg-cyan-500 text-white h-screen'>
-      <div>Hello, world.</div>
-      <div>Welcome to World Records!</div>
+    <div className='grid grid-rows-[200px_200px] text-2xl font-bold bg-cyan-500 text-white h-screen'>
+      <div className='flex center'>Hello, world.</div>
+      <div className='flex center'>Welcome to World Records!</div>
     </div>
   )
 }
